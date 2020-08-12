28333273bf0d746aea4ba2a26dbcdcaafd7611c5
MaxKicw <m142679@googlemail.com>
2020-08-12 14:38:41 +0200

    save order

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -1,11 +1,27 @@
 import {stripe} from './'
 import Stripe from 'stripe';
+import axios from 'axios';
 
 
 const webhookHandlers = {
 
     'payment_intent.succeeded': async (data: Stripe.PaymentIntent) => {
-      console.log("Super hat geklappt"+JSON.stringify(data));
+      console.log("Super hat geklappt"+JSON.stringify(data.metadata));
+      axios.post('https://www.wallpastry.com/orders', {
+        orderid:data.id,
+        email:data.receipt_email,
+        name: data.shipping.name,
+        adress:data.shipping.address.line1,
+        city:data.shipping.address.postal_code+"/"+data.shipping.address.city,
+        order:JSON.parse(data.metadata.order),
+        amount:data.amount_received/100
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     },
     'payment_intent.payment_failed': async (data: Stripe.PaymentIntent) => {
         console.log("Shit hat nicht geklappt")
